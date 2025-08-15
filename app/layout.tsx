bf53dd08e18c8a16228a076982c5cd1375a8bd08
Shawn Vibert <humanmaximuscodex@gmail.com>
2025-08-15 16:46:52 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 export const metadata = { title: "SmartNET • Hub (v17)", description: "SmartNET Hub v17" };
 import "../styles/globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="dark" suppressHydrationWarning>
       <body>{children}</body>
